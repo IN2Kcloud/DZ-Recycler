c94e11172efe603482d75f6cb7c28cc30f3fe90e
SINXSIN <131200320+IN2Kcloud@users.noreply.github.com>
2024-04-17 20:25:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,6 +37,15 @@ gsap.from(".nlinkII",{
   opacity: 0
 })  
 
+// Check if the device is a desktop
+function isDesktop() {
+  // Regular expression to match common desktop user agents
+  const desktopRegex = /Windows NT|Macintosh/;
+  return desktopRegex.test(navigator.userAgent);
+}
+
+// Apply image effect only on desktop devices
+if (isDesktop()) {
 Shery.imageEffect("#heroc img",{
   style: 3,
   config: {"uFrequencyX":{"value":14.5,"range":[0,100]},"uFrequencyY":{"value":14.5,"range":[0,100]},"uFrequencyZ":{"value":14.5,"range":[0,100]},"geoVertex":{"range":[1,64],"value":40.44},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.681823856621676},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0.5,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":false},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.55,"range":[1,5]},"scrollType":{"value":0},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.002,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
@@ -66,3 +75,4 @@ Shery.imageEffect('.Gooey', {
     uniforms.noEffectGooey.value = false
   }
 })
+}
